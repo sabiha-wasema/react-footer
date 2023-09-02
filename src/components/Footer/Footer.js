8aa75fd067ac9dec5744c00ab263cd233c00758d
sabiha-wasema <sabihawasemaa1244@gmail.com>
2023-09-02 21:09:03 +0600

    header component create

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,99 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-dark text-light py-4">
+      <div className="container">
+        <div className="row g-4">
+          {" "}{/* Added "g-4" class for column gap */}
+          <div className="col-md-4 d-flex flex-column">
+            <div className="d-flex align-items-center">
+              <i class="fa-solid fa-paper-plane mb-1 text-success" />
+              <h5 className="ms-3">E-Coaching</h5>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+              quidem explicabo voluptate neque laboriosam, perferendis
+              consectetur modi repudiandae doloribus harum?
+            </p>
+            <div className="input-group ">
+              <input
+                type="text"
+                className=" form-control bg-dark py-2 text-white"
+                placeholder="Enter the Email"
+                style={{ borderColor: "#4ddd7f" }}
+              />
+              <button
+                className="btn btn-outline-primary px-3"
+                type="button"
+                style={{
+                  backgroundColor: "#4ddd7f",
+                  borderColor: "#4ddd7f",
+                  color: "white"
+                }}
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <h5>Payment Methods</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Link 4</a>
+              </li>
+              <li>
+                <a href="#">Link 5</a>
+              </li>
+              <li>
+                <a href="#">Link 6</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5>Quick Link</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Link 7</a>
+              </li>
+              <li>
+                <a href="#">Link 8</a>
+              </li>
+              <li>
+                <a href="#">Link 9</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5>Services</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Link 10</a>
+              </li>
+              <li>
+                <a href="#">Link 11</a>
+              </li>
+              <li>
+                <a href="#">Link 12</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5>Support</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#">Link 10</a>
+              </li>
+              <li>
+                <a href="#">Link 11</a>
+              </li>
+              <li>
+                <a href="#">Link 12</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,50 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <nav class="navbar navbar-expand-lg bg-color ">
+        <div class="container ">
+          <a class="navbar-brand text-white" href="#">
+            E-Coaching
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a
+                  class="nav-link active text-white"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">
+                  Contact
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
