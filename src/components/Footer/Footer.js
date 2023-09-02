0ff8607005e67d3a4dde8970e49c1bf15b1ee227
sabiha-wasema <sabihawasemaa1244@gmail.com>
2023-09-03 01:36:26 +0600

    add comment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,14 +10,17 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-light">
       <div className="container py-4 py-md-5">
+        {/* Footer Description Section */}
         <div className="row g-4 g-md-5 footer-description">
           {" "}{/* Added "g-4" class for column gap */}
+          {/* E-Coaching Section */}
           <div className="col-md-4 col-sm-6 d-flex flex-column">
             <div className="d-flex align-items-center">
               <i class="fa-solid fa-paper-plane mb-2 custom-icon" />
               <h6 className="ms-2 custom-heading">E - Coaching</h6>
             </div>
             <p className="small-text">
+              {/* Description of E-Coaching */}
               Coaching is a means for developing a partnership between the
               manager and employee that creates a shared understanding about
               what needs to be achieved and how it is to be achieved.
@@ -42,6 +45,7 @@ const Footer = () => {
               </button>
             </div>
           </div>
+          {/* Payment Methods Section */}
           <div className="col-md-2 col-sm-6">
             <h5 className="mb-3 mb-md-2">Payment Methods</h5>
             <div className="row">
@@ -80,6 +84,7 @@ const Footer = () => {
               </div>
             </div>
           </div>
+          {/* Quick Links Section */}
           <div className="col-md-2 col-sm-6 mb-3">
             <h5 className="mb-3 ms-2 mb-md-2">Quick Link</h5>
             <ul className="list-unstyled">
@@ -111,6 +116,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
+          {/* Services Section */}
           <div className="col-md-2 col-sm-6 mb-3">
             <h5 className="ms-2 mb-3 mb-md-2">Services</h5>
             <ul className="list-unstyled">
@@ -141,6 +147,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
+          {/* Support Section */}
           <div className="col-md-2 col-sm-6 mb-3">
             <h5 className="ms-2 mb-3 mb-md-2">Support</h5>
             <ul className="list-unstyled">
@@ -168,19 +175,23 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      {/* Custom Container */}
       <div className="custom-container">
         <div className="row py-2 py-md-1">
+          {/* Copyright Section */}
           <div className="col-md-5 col-sm-12 custom-col text-center text-md-start">
             <div class="copyright copyright-small-text">
               Copyright &copy; {new Date().getFullYear()} Educational Center
             </div>
           </div>
+          {/* Contact Info Section */}
           <div className="col-md-3 col-sm-6 custom-col text-center text-md-start">
             <div class="contact-info">
               <i class=" mb-2 mb-md-3 fa-solid fa-envelope" />
               <p>info@educationalcenter.com</p> {/* Email address */}
             </div>
           </div>
+          {/* Phone Info Section */}
           <div className="col-md-2 col-sm-6 custom-col text-center text-md-start">
             <div class="contact-info">
               <i class="mb-2 mb-md-3 fa-solid fa-phone-volume" />{" "}
@@ -188,6 +199,7 @@ const Footer = () => {
               <p>+1-774-267-8439</p>
             </div>
           </div>
+          {/* Social Icons Section */}
           <div className="col-md-2 col-sm-12 custom-col text-center text-md-start">
             <div class="social-icons">
               <a href="#">
